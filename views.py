11b03c05ebf15f925330da947226a66e8c18b4c5
basharaz1 <basharalli@h-MacBook-Pro-sl-Bashar.local>
2024-05-20 11:29:04 +0300

    adding url and html

--- views.py
+++ views.py
@@ -1,9 +1,8 @@
-from flask import render_template, request, redirect, flash, url_for,send_file
+from flask import render_template, request, redirect, flash, url_for
 from models import User, db
-from sqlalchemy.exc import IntegrityError
 import boto3
-from botocore.exceptions import NoCredentialsError, PartialCredentialsError
-from io import BytesIO
+from sqlalchemy.exc import IntegrityError
+
 
 
 
@@ -24,17 +23,17 @@ def homepage():
 
 
 
+
+
 def welcome(username):
     bucket_name = 'labkaliedoo19980'
     object_name = 'Israel-1200-BarLev.png'
     region = 'us-west-2'
+    s3 = boto3.client('s3')
 
-    s3 = boto3.client('s3', region_name=region)
     try:
-        s3_response = s3.get_object(Bucket=bucket_name, Key=object_name)
-        image_data = s3_response['Body'].read()
-        return send_file(BytesIO(image_data), download_name='image.png', mimetype='image/png')
-    except (NoCredentialsError, PartialCredentialsError):
-        return "Error retrieving the object due to credential issues", 500
+        image_url = s3.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': object_name}, ExpiresIn=3600)
     except Exception as e:
-        return f"Error retrieving the object: {str(e)}", 500
+        return f"Error generating presigned URL: {str(e)}", 500
+
+    return render_template('welcome.html', username=username, image_url=image_url)
